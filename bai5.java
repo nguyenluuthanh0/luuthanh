3584041a5ffa17cc4d044a0d7c4de4408902dc84
nguyenluuthanh0 <125626967+nguyenluuthanh0@users.noreply.github.com>
2023-03-14 17:16:23 +0700

    Create bai5.java

---
+++ bai5.java
@@ -0,0 +1,14 @@
+package bai5;
+import java.util.Scanner;
+public class bai5 {
+	public static void main(String[] args) {
+        int n,S = 0;
+        Scanner input = new Scanner(System.in);
+        while(S <= 100) {
+            System.out.println("Nhap 1 so nguyen:");
+            n = input.nextInt();
+            S += n;
+        }
+        System.out.println("Tong cac so da nhap la " + S);
+    }
+}	
