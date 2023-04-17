39dbf8efd8a58b62b3598db53fe61fc1aa21427d
nguyenluuthanh0 <125626967+nguyenluuthanh0@users.noreply.github.com>
2023-04-17 18:04:15 +0700

    Create MAIN.java

---
+++ MAIN.java
@@ -0,0 +1,16 @@
+package MAIN;
+
+public class MAIN {
+	
+	    public static void main(String[] args) {
+	        // Khai báo đối tượng Sach
+	        SACH sach = new SACH();
+
+	        // Nhập thông tin 1 cuốn sách và hiển thị thông tin cuốn sách vừa nhập
+	        System.out.println("Nhap thong tin cuon sach:");
+	        sach.nhapSach();
+	        System.out.println("\nThong tin cuon sach vua nhap:");
+	        sach.inSach();
+	    }
+	    
+	}
